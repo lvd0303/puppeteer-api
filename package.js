ba6ed2f191d67c480b6a8bb07bbf2ecc7660b93b
lvd0303 <lvd0303@gmail.com>
2025-02-13 18:26:54 +0700

    Create package.js

---
+++ package.js
@@ -0,0 +1,13 @@
+{
+  "name": "puppeteer-api",
+  "version": "1.0.0",
+  "description": "Puppeteer API for scraping",
+  "main": "server.js",
+  "dependencies": {
+    "express": "^4.17.1",
+    "puppeteer-core": "^19.0.0"
+  },
+  "scripts": {
+    "start": "node server.js"
+  }
+}
